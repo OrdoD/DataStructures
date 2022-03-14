21b436e78bedcb29b7033822dac9d33a7777b352
Daniel Ordonez <daniel.ordonez210@gmail.com>
2022-03-14 13:31:58 -0700

    Made changes to homework

--- CS297_3-10-2022 (Homework 6 - AVL Tree)/DanielOrdonez_AVL.cpp
+++ CS297_3-10-2022 (Homework 6 - AVL Tree)/DanielOrdonez_AVL.cpp
@@ -1,5 +1,7 @@
 #include <string>
 #include <iostream>
+#include <vector>
+#include <vector>
 
 
 #define COUNT 15
@@ -125,33 +127,29 @@ Node<T>* Node<T>::insert(Node<T>* node, T key)
     int balance = getBalance(node);
 
 
-
-    // Left Left Case
     if (balance > 1 && key < node->left->key) {
         return rightRotate(node);
     }
 
 
-    // Right Right Case
     if (balance < -1 && key > node->right->key) {
         return leftRotate(node);
     }
 
-    // Left Right Case
+
     if (balance > 1 && key > node->left->key)
     {
         node->left = leftRotate(node->left);
         return rightRotate(node);
     }
 
-    // Right Left Case
     if (balance < -1 && key < node->right->key)
     {
         node->right = rightRotate(node->right);
         return leftRotate(node);
     }
 
-    /* return the (unchanged) node pointer */
+
     return node;
 }
 
@@ -169,18 +167,17 @@ void Node<T>::preOrder(Node<T>* root)
 template <typename T>
 void Node<T>::print2DUtil(Node<T>* root, int space)
 {
-    // Base case
+
     if (root == NULL)
         return;
 
-    // Increase distance between levels
+   
     space += COUNT;
 
-    // Process right child first
+
     print2DUtil(root->right, space);
 
-    // Print current node after space
-    // count
+
     std::cout << std::endl;
     for (int i = COUNT; i < space; i++)
         std::cout << " ";
@@ -191,7 +188,7 @@ void Node<T>::print2DUtil(Node<T>* root, int space)
 }
 
 template <typename T>
-// Wrapper over print2DUtil()
+
 void Node<T>::print2D(Node<T>* root)
 {
     // Pass initial space count as 0
@@ -203,7 +200,6 @@ Node<T>* Node<T>::minValueNode(Node<T>* node)
 {
     Node<T>* current = node;
 
-    /* loop down to find the leftmost leaf */
     while (current->left != NULL)
         current = current->left;
 
@@ -214,27 +210,21 @@ template <typename T>
 Node<T>* Node<T>::deleteNode(Node<T>* root, T key)
 {
 
-    // STEP 1: PERFORM STANDARD BST DELETE
+
     if (root == NULL)
         return root;
 
-    // If the key to be deleted is smaller
-    // than the root's key, then it lies
-    // in left subtree
+
     if (key < root->key)
         root->left = deleteNode(root->left, key);
 
-    // If the key to be deleted is greater
-    // than the root's key, then it lies
-    // in right subtree
     else if (key > root->key)
         root->right = deleteNode(root->right, key);
 
-    // if key is same as root's key, then
-    // This is the node to be deleted
+
     else
     {
-        // node with only one child or no child
+
         if ((root->left == NULL) ||
             (root->right == NULL))
         {
@@ -242,56 +232,48 @@ Node<T>* Node<T>::deleteNode(Node<T>* root, T key)
                 root->left :
                 root->right;
 
-            // No child case
             if (temp == NULL)
             {
                 temp = root;
                 root = NULL;
             }
-            else // One child case
-                *root = *temp; // Copy the contents of
-                               // the non-empty child
-            free(temp);
+            else {
+
+                *root = *temp;
+                free(temp);
+            }
+                
         }
         else
         {
-            // node with two children: Get the inorder
-            // successor (smallest in the right subtree)
+          
             Node<T>* temp = minValueNode(root->right);
 
-            // Copy the inorder successor's
-            // data to this node
+       
             root->key = temp->key;
 
-            // Delete the inorder successor
             root->right = deleteNode(root->right,
                 temp->key);
         }
     }
 
-    // If the tree had only one node
-    // then return
+
     if (root == NULL)
         return root;
 
-    // STEP 2: UPDATE HEIGHT OF THE CURRENT NODE
+
     root->height = 1 + max(getHeight(root->left),
         getHeight(root->right));
 
-    // STEP 3: GET THE BALANCE FACTOR OF
-    // THIS NODE (to check whether this
-    // node became unbalanced)
+
     int balance = getBalance(root);
 
-    // If this node becomes unbalanced,
-    // then there are 4 cases
 
-    // Left Left Case
     if (balance > 1 &&
         getBalance(root->left) >= 0)
         return rightRotate(root);
 
-    // Left Right Case
+
     if (balance > 1 &&
         getBalance(root->left) < 0)
     {
@@ -299,12 +281,12 @@ Node<T>* Node<T>::deleteNode(Node<T>* root, T key)
         return rightRotate(root);
     }
 
-    // Right Right Case
+
     if (balance < -1 &&
         getBalance(root->right) <= 0)
         return leftRotate(root);
 
-    // Right Left Case
+
     if (balance < -1 &&
         getBalance(root->right) > 0)
     {
@@ -320,33 +302,18 @@ int main() {
 
     Node<std::string>* root = NULL;
 
-    /* Constructing tree given in
-    the above figure */
-
-    std::string presidents[] = { "Harry S.Truman", "Dwight D.Eisenhower",
+    std::vector<std::string> presidents = { "Harry S.Truman", "Dwight D.Eisenhower",
             "John F.Kennedy", "Lyndon B.Johnson", "Richard M.Nixon",
             "Gerald R.Ford", "Jimmy Carter", "Ronald Reagan", "George Bush",
             "Bill Clinton", "George W.Bush", "Barak Obama", "Donald J.Trump", "Joseph R.Biden" };
 
-    int size = sizeof(presidents) / sizeof(presidents[0]);
 
+    for (auto i = 0; i < presidents.size(); i++) {
 
-    for (auto i = 0; i < size; i++) {
             root = root->insert(root, presidents[i]);
+
         }
 
-   
-  
-
-    /* The constructed AVL Tree would be
-                30
-            / \
-            20 40
-            / \ \
-        10 25 50
-    */
-    std::cout << "Preorder traversal of the "
-        "constructed AVL tree is \n";
 
     root->print2D(root);
 
