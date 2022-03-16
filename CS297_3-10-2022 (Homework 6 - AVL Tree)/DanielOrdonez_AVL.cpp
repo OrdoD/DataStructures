0eab64fceedb29deb04fb89a20c928c7e8a15f16
DanO <dordonez@usv.edu>
2022-03-16 11:44:54 -0700

    Made changs to the AVL tree homework

--- CS297_3-10-2022 (Homework 6 - AVL Tree)/DanielOrdonez_AVL.cpp
+++ CS297_3-10-2022 (Homework 6 - AVL Tree)/DanielOrdonez_AVL.cpp
@@ -1,10 +1,6 @@
 #include <string>
 #include <iostream>
 #include <vector>
-#include <vector>
-
-
-#define COUNT 15
 
 template <typename T>
 class Node
@@ -14,16 +10,14 @@ public:
     Node<T>* left;
     Node<T>* right;
     int height;
-    int getHeight(Node<T>* N);
-    int max(int a, int b);
+    unsigned int getHeight(Node<T>* N);
+    unsigned int max(int a, int b);
     Node<T>* newNode(T key);
     Node<T>* rightRotate(Node<T>* y);
     Node<T>* leftRotate(Node<T>* x);
-    int getBalance(Node<T>* N);
+    unsigned int getBalance(Node<T>* N);
     Node<T>* insert(Node<T>* node, T key);
     void preOrder(Node<T>* root);
-    void print2DUtil(Node<T>* root, int space);
-    void print2D(Node<T>* root);
     Node<T>* deleteNode(Node<T>* root, T key);
     Node<T>* minValueNode(Node<T>* node);
 };
@@ -32,7 +26,7 @@ public:
 int max(int a, int b);
 
 template <typename T>
-int Node<T>::getHeight(Node<T>* N)
+unsigned int Node<T>::getHeight(Node<T>* N)
 {
     if (N == NULL)
         return 0;
@@ -40,7 +34,7 @@ int Node<T>::getHeight(Node<T>* N)
 }
 
 template <typename T>
-int Node<T>::max(int a, int b)
+unsigned int Node<T>::max(int a, int b)
 {
     return (a > b) ? a : b;
 }
@@ -53,7 +47,7 @@ Node<T>* Node<T>::newNode(T key)
     node->key = key;
     node->left = NULL;
     node->right = NULL;
-    node->height = 1; 
+    node->height = 1;
     return(node);
 }
 
@@ -96,7 +90,7 @@ Node<T>* Node<T>::leftRotate(Node<T>* x)
     return y;
 }
 template <typename T>
-int Node<T>::getBalance(Node<T>* N)
+unsigned int Node<T>::getBalance(Node<T>* N)
 {
     if (N == NULL)
         return 0;
@@ -108,7 +102,7 @@ int Node<T>::getBalance(Node<T>* N)
 template <typename T>
 Node<T>* Node<T>::insert(Node<T>* node, T key)
 {
-   
+
     if (node == NULL)
         return(newNode(key));
 
@@ -116,10 +110,10 @@ Node<T>* Node<T>::insert(Node<T>* node, T key)
         node->left = insert(node->left, key);
     else if (key > node->key)
         node->right = insert(node->right, key);
-    else 
+    else
         return node;
 
-   
+
     node->height = 1 + max(getHeight(node->left),
         getHeight(node->right));
 
@@ -164,36 +158,6 @@ void Node<T>::preOrder(Node<T>* root)
     }
 }
 
-template <typename T>
-void Node<T>::print2DUtil(Node<T>* root, int space)
-{
-
-    if (root == NULL)
-        return;
-
-   
-    space += COUNT;
-
-
-    print2DUtil(root->right, space);
-
-
-    std::cout << std::endl;
-    for (int i = COUNT; i < space; i++)
-        std::cout << " ";
-    std::cout << root->key << "\n";
-
-    // Process left child
-    print2DUtil(root->left, space);
-}
-
-template <typename T>
-
-void Node<T>::print2D(Node<T>* root)
-{
-    // Pass initial space count as 0
-    print2DUtil(root, 0);
-}
 
 template <typename T>
 Node<T>* Node<T>::minValueNode(Node<T>* node)
@@ -242,14 +206,14 @@ Node<T>* Node<T>::deleteNode(Node<T>* root, T key)
                 *root = *temp;
                 free(temp);
             }
-                
+
         }
         else
         {
-          
+
             Node<T>* temp = minValueNode(root->right);
 
-       
+
             root->key = temp->key;
 
             root->right = deleteNode(root->right,
@@ -310,16 +274,21 @@ int main() {
 
     for (auto i = 0; i < presidents.size(); i++) {
 
-            root = root->insert(root, presidents[i]);
+        root = root->insert(root, presidents[i]);
 
-        }
+    }
 
-
-    root->print2D(root);
+    std::cout << "Before Deleting Of An Element" << "\n";
+    root->preOrder(root);
 
     root->deleteNode(root, "Lyndon B.Johnson");
-    std::cout << "\n\n\n\n\n\n\n\n\n\n"; 
-    root->print2D(root);
+
+    std::cout << "\n\n";
+
+    std::cout << "After Deleting Of An Element" << "\n";
+    root->preOrder(root);
+
+    std::cout << "\n\n";
 
     return 0;
 
